5ab52d0746041636c58f7303473c958565402a1a
Jonathan Silverman <jsilver@mindynamics.com>
2010-04-22 17:35:42 -0700

    languages

--- app/helpers/pastes_helper.rb
+++ app/helpers/pastes_helper.rb
@@ -1,2 +1,28 @@
 module PastesHelper
+
+  def languages
+    %w(
+Ruby
+C
+C++
+CSS
+Delphi
+diff
+Groovy
+HTML
+Java
+JavaScript
+JSON
+Nitro-XHTML
+PHP
+Python
+RHTML
+Scheme
+SQL
+XML
+YAML
+    )
+  end
+
 end
+
