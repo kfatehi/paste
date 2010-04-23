912f3ccdbd60d7c98c95fe04208529426381f472
Keyvan Fatehi <keyvan@mindynamics.com>
2010-04-22 22:18:37 -0700

    shortened the urls

--- config/routes.rb
+++ config/routes.rb
@@ -40,6 +40,8 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing or commenting them out if you're using named routes and resources.
+  map.root :controller=>"pastes", :aciton=>"index"
+  map.connect ':id', :controller=>"pastes", :action=>"show"
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
 end
