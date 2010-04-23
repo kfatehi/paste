62f993af7b46f9a7ab618d8a38e215159951adc5
Keyvan Fatehi <keyvan@mindynamics.com>
2010-04-22 23:42:01 -0700

    lots of changes

--- app/helpers/pastes_helper.rb
+++ app/helpers/pastes_helper.rb
@@ -23,6 +23,19 @@ XML
 YAML
     )
   end
-
+  
+  def html_title(id=nil)
+    id.nil? ? "pastebot: #mindynamics @ irc.freenode.net" : "paste ##{id}"
+  end
+  
+  def toggle(switch)
+    switch ||= '0'
+    switch.to_i > 0 ? '?' : '?line_numbers=1'
+  end
+  
+  def paste_title(s)
+    s.nil? || s.empty? || s == 'untitled' ? return : "entitled \"#{s}\""
+  end
+  
 end
 
